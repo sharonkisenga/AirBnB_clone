319016fdbc46f23f0822f3c9b15155b410508a43
Coleynick <ezechidinma14@gmail.com>
2023-09-15 21:26:09 -0700

    Update

---
+++ models/engine/file_storage.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""
+A storage class Module.
+"""
+import json
+import os
+from models.base_model import BaseModel
+from models import State, City, Amenity, Place, Review
+
+
+class FileStorage:
+    """Class to serialize & deserialize insts to/from a JSON file."""
+
+    __file_path = "file.json"
+    __objects = {}
+    __classes = {
+        'BaseModel': BaseModel,
+        'User': User,
+        'State': State,
+        'City': City,
+        'Amenity': Amenity,
+        'Place': Place,
+        'Review': Review
+    }
+
+    def all(self):
+        """
+        Retrieve all objs stored in the storage.
+
+        Returns:
+            dict: A dict.
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        Add a new obj to d storage.
+
+        Args:
+            obj (BaseModel): D obj to be added.
+        """
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """
+        Serialize & save d objs to d JSON file.
+        """
+        seralize = {}
+        for key, value in self.__objects.items():
+            seralize[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding='utf-8') as ifile:
+            json.dump(seralize, ifile)
+
+    def reload(self):
+        """
+        Deserialize & load objs from d JSON file.
+        """
+        try:
+            with open(self.__file_path, 'r', encoding='utf-8') as ifile:
+                obj_dict = json.load(ifile)
+                for key, value in obj_dict.items():
+                    class_name, obj_id = key.split('.')
+                    self.__objects[key] = self.__classes[class_name](**value)
+        except FileNotFoundError:
+            pass
