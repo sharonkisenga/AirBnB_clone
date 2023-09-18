a2dc646cb8a9fce979dd479125cd3a793875709d
sharonkisenga <sharonglor69@gmail.com>
2023-09-18 07:43:19 -0700

    git

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,157 +1,56 @@
 #!/usr/bin/python3
-"""Test BaseModel documentation"""
-from datetime import datetime
-import inspect
-import models
-import pep8 as pycodestyle
-import time
+"""Unittest for BaseModel class"""
 import unittest
-from unittest import mock
-BaseModel = models.base_model.BaseModel
-module_doc = models.base_model.__doc__
-
-
-class TestBaseModelDocs(unittest.TestCase):
-    """Test that save method updates"""
-
-    @classmethod
-    def setUpClass(self):
-        """Test that save method updates"""
-        self.base_funcs = inspect.getmembers(BaseModel, inspect.isfunction)
-
-    def test_pep8_conformance(self):
-        """Test that save method updates"""
-        for path in ['models/base_model.py',
-                     'tests/test_models/test_base_model.py']:
-            with self.subTest(path=path):
-                errors = pycodestyle.Checker(path).check_all()
-                self.assertEqual(errors, 0)
-
-    def test_module_docstring(self):
-        """Test that save method updates"""
-        self.assertIsNot(module_doc, None,
-                         "base_model.py needs a docstring")
-        self.assertTrue(len(module_doc) > 1,
-                        "base_model.py needs a docstring")
-
-    def test_class_docstring(self):
-        """Test that save method updates"""
-        self.assertIsNot(BaseModel.__doc__, None,
-                         "BaseModel class needs a docstring")
-        self.assertTrue(len(BaseModel.__doc__) >= 1,
-                        "BaseModel class needs a docstring")
-
-    def test_func_docstrings(self):
-        """Test that save method updates"""
-        for func in self.base_funcs:
-            with self.subTest(function=func):
-                self.assertIsNot(
-                    func[1].__doc__,
-                    None,
-                    "{:s} method needs a docstring".format(func[0])
-                )
-                self.assertTrue(
-                    len(func[1].__doc__) > 1,
-                    "{:s} method needs a docstring".format(func[0])
-                )
+import os
+from models.base_model import BaseModel
+from datetime import datetime
 
 
 class TestBaseModel(unittest.TestCase):
-    """Test that save method updates"""
-    def test_instantiation(self):
-        """Test that save method updates"""
-        inst = BaseModel()
-        self.assertIs(type(inst), BaseModel)
-        inst.name = "Holberton"
-        inst.number = 89
-        attrs_types = {
-            "id": str,
-            "created_at": datetime,
-            "updated_at": datetime,
-            "name": str,
-            "number": int
+    """Test cases for BaseModel class"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Class method to open test's environment"""
+        cls.base_model = BaseModel()
+        try:
+            os.rename("file.json", "test_file.json")
+        except Exception:
+            pass
+
+    @classmethod
+    def tearDownClass(cls):
+        """Class method to close test's environment"""
+        try:
+            os.remove("file.json")
+            os.rename("test_file.json", "file.json")
+        except Exception:
+            pass
+
+    def test_save_method(self):
+        """Test case for 'save' method"""
+        datetime_prev = self.base_model.updated_at
+        self.base_model.save()
+        self.assertGreater(self.base_model.updated_at, datetime_prev)
+        self.assertTrue(os.path.exists("file.json"))
+
+    def test_str_method(self):
+        """Test case for str instance representation"""
+        cls_name = str(self.base_model.__class__.__name__)
+        obj_dict = str(self.base_model.__dict__)
+        obj_str = f"[{cls_name}] ({self.base_model.id}) {obj_dict}"
+        self.assertEqual(obj_str, self.base_model.__str__())
+
+    def test_to_dict_method(self):
+        """Test case for 'to_dict' method"""
+        dict = {
+            "id": self.base_model.id,
+            "__class__": self.base_model.__class__.__name__,
+            "created_at": self.base_model.created_at.isoformat(),
+            "updated_at": self.base_model.updated_at.isoformat()
         }
-        for attr, typ in attrs_types.items():
-            with self.subTest(attr=attr, typ=typ):
-                self.assertIn(attr, inst.__dict__)
-                self.assertIs(type(inst.__dict__[attr]), typ)
-        self.assertEqual(inst.name, "Holberton")
-        self.assertEqual(inst.number, 89)
+        self.assertDictEqual(dict, self.base_model.to_dict())
 
-    def test_datetime_attributes(self):
-        """Test that save method updates"""
-        tic = datetime.now()
-        inst1 = BaseModel()
-        toc = datetime.now()
-        self.assertTrue(tic <= inst1.created_at <= toc)
-        time.sleep(1e-4)
-        tic = datetime.now()
-        inst2 = BaseModel()
-        toc = datetime.now()
-        self.assertTrue(tic <= inst2.created_at <= toc)
-        self.assertEqual(inst1.created_at, inst1.updated_at)
-        self.assertEqual(inst2.created_at, inst2.updated_at)
-        self.assertNotEqual(inst1.created_at, inst2.created_at)
-        self.assertNotEqual(inst1.updated_at, inst2.updated_at)
 
-    def test_uuid(self):
-        """Test that save method updates"""
-        inst1 = BaseModel()
-        inst2 = BaseModel()
-        for inst in [inst1, inst2]:
-            uuid = inst.id
-            with self.subTest(uuid=uuid):
-                self.assertIs(type(uuid), str)
-                self.assertRegex(uuid,
-                                 '^[0-9a-f]{8}-[0-9a-f]{4}'
-                                 '-[0-9a-f]{4}-[0-9a-f]{4}'
-                                 '-[0-9a-f]{12}$')
-        self.assertNotEqual(inst1.id, inst2.id)
-
-    def test_to_dict(self):
-        """Test that save method updates"""
-        my_model = BaseModel()
-        my_model.name = "Holberton"
-        my_model.my_number = 89
-        d = my_model.to_dict()
-        expected_attrs = ["id",
-                          "created_at",
-                          "updated_at",
-                          "name",
-                          "my_number",
-                          "__class__"]
-        self.assertCountEqual(d.keys(), expected_attrs)
-        self.assertEqual(d['__class__'], 'BaseModel')
-        self.assertEqual(d['name'], "Holberton")
-        self.assertEqual(d['my_number'], 89)
-
-    def test_to_dict_values(self):
-        """Test that save method updates"""
-        t_format = "%Y-%m-%dT%H:%M:%S.%f"
-        bm = BaseModel()
-        new_d = bm.to_dict()
-        self.assertEqual(new_d["__class__"], "BaseModel")
-        self.assertEqual(type(new_d["created_at"]), str)
-        self.assertEqual(type(new_d["updated_at"]), str)
-        self.assertEqual(new_d["created_at"], bm.created_at.strftime(t_format))
-        self.assertEqual(new_d["updated_at"], bm.updated_at.strftime(t_format))
-
-    def test_str(self):
-        """Test that save method updates"""
-        inst = BaseModel()
-        string = "[BaseModel] ({}) {}".format(inst.id, inst.__dict__)
-        self.assertEqual(string, str(inst))
-
-    @mock.patch('models.storage')
-    def test_save(self, mock_storage):
-        """Test that save method updates"""
-        inst = BaseModel()
-        old_created_at = inst.created_at
-        old_updated_at = inst.updated_at
-        inst.save()
-        new_created_at = inst.created_at
-        new_updated_at = inst.updated_at
-        self.assertNotEqual(old_updated_at, new_updated_at)
-        self.assertEqual(old_created_at, new_created_at)
-        self.assertTrue(mock_storage.new.called)
-        self.assertTrue(mock_storage.save.called)
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,121 +1,37 @@
 #!/usr/bin/python3
-"""the TestReviewDocs classes"""
-
-from datetime import datetime
-import inspect
-import models
-from models import review
-from models.base_model import BaseModel
-import pep8
+"""Unittest for Review class"""
 import unittest
-Review = review.Review
-
-
-class TestReviewDocs(unittest.TestCase):
-    """test that do the review with correct output"""
-    @classmethod
-    def setUpClass(cls):
-        """test that do the review with correct output"""
-        cls.review_f = inspect.getmembers(Review, inspect.isfunction)
-
-    def test_pep8_conformance_review(self):
-        """test that do the review with correct output"""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['models/review.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-    def test_pep8_conformance_test_review(self):
-        """test that do the review with correct output"""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['tests/test_models/test_review.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-    def test_review_module_docstring(self):
-        """test that do the review with correct output"""
-        self.assertIsNot(review.__doc__, None,
-                         "review.py needs a docstring")
-        self.assertTrue(len(review.__doc__) >= 1,
-                        "review.py needs a docstring")
-
-    def test_review_class_docstring(self):
-        """test that do the review with correct output"""
-        self.assertIsNot(Review.__doc__, None,
-                         "Review class needs a docstring")
-        self.assertTrue(len(Review.__doc__) >= 1,
-                        "Review class needs a docstring")
-
-    def test_review_func_docstrings(self):
-        """Test for the presence of docstrings in Review methods"""
-        for func in self.review_f:
-            self.assertIsNot(func[1].__doc__, None,
-                             "{:s} method needs a docstring".format(func[0]))
-            self.assertTrue(len(func[1].__doc__) >= 1,
-                            "{:s} method needs a docstring".format(func[0]))
+import os
+from models.review import Review
 
 
 class TestReview(unittest.TestCase):
-    """Test the unitest review"""
-    def test_is_subclass(self):
-        """test that do the review with correct output"""
-        review = Review()
-        self.assertIsInstance(review, BaseModel)
-        self.assertTrue(hasattr(review, "id"))
-        self.assertTrue(hasattr(review, "created_at"))
-        self.assertTrue(hasattr(review, "updated_at"))
+    """Test cases for Review class"""
 
-    def test_place_id_attr(self):
-        """test that do the review with correct output"""
-        review = Review()
-        self.assertTrue(hasattr(review, "place_id"))
-        if models.storage_t == 'db':
-            self.assertEqual(review.place_id, None)
-        else:
-            self.assertEqual(review.place_id, "")
+    @classmethod
+    def setUpClass(cls):
+        """Class method to open test's environment"""
+        cls.reviewInstance = Review()
+        try:
+            os.rename("file.json", "test_file.json")
+        except Exception:
+            pass
 
-    def test_user_id_attr(self):
-        """test that do the review with correct output"""
-        review = Review()
-        self.assertTrue(hasattr(review, "user_id"))
-        if models.storage_t == 'db':
-            self.assertEqual(review.user_id, None)
-        else:
-            self.assertEqual(review.user_id, "")
+    @classmethod
+    def tearDownClass(cls):
+        """Class method to close test's environment"""
+        try:
+            os.remove("file.json")
+            os.rename("test_file.json", "file.json")
+        except Exception:
+            pass
 
-    def test_text_attr(self):
-        """test that do the review with correct output"""
-        review = Review()
-        self.assertTrue(hasattr(review, "text"))
-        if models.storage_t == 'db':
-            self.assertEqual(review.text, None)
-        else:
-            self.assertEqual(review.text, "")
+    def test_attrs(self):
+        """Test case for 'Review' class attributes"""
+        self.assertEqual(self.reviewInstance.place_id, "")
+        self.assertEqual(self.reviewInstance.user_id, "")
+        self.assertEqual(self.reviewInstance.text, "")
 
-    def test_to_dict_creates_dict(self):
-        """test that do the review with correct output"""
-        r = Review()
-        new_d = r.to_dict()
-        self.assertEqual(type(new_d), dict)
-        self.assertFalse("_sa_instance_state" in new_d)
-        for attr in r.__dict__:
-            if attr is not "_sa_instance_state":
-                self.assertTrue(attr in new_d)
-        self.assertTrue("__class__" in new_d)
 
-    def test_to_dict_values(self):
-        """test that do the review with correct output"""
-        t_format = "%Y-%m-%dT%H:%M:%S.%f"
-        r = Review()
-        new_d = r.to_dict()
-        self.assertEqual(new_d["__class__"], "Review")
-        self.assertEqual(type(new_d["created_at"]), str)
-        self.assertEqual(type(new_d["updated_at"]), str)
-        self.assertEqual(new_d["created_at"], r.created_at.strftime(t_format))
-        self.assertEqual(new_d["updated_at"], r.updated_at.strftime(t_format))
-
-    def test_str(self):
-        """test that do the review with correct output"""
-        review = Review()
-        string = "[Review] ({}) {}".format(review.id, review.__dict__)
-        self.assertEqual(string, str(review))
+if __name__ == "__main__":
+    unittest.main()
